ef2dedaea42c0774597b99a91c4f462c5592d9a7
Dmitriy <120090144+DmitriyTereschenko89@users.noreply.github.com>
2024-11-14 16:54:56 +0300

    Update DStudio Query.sql

--- DStudio Query.sql
+++ DStudio Query.sql
@@ -1,6 +1,6 @@
 use [DStudio.Calmplete]
 
---Напишите запрос, чтобы выбрать всех пользователей с неподтвержденными электронными адресами;
+--Write a query to select all users with unconfirmed emails;;
 select *
 from Users
 where EmailConfirmed = 0
@@ -8,15 +8,15 @@ where EmailConfirmed = 0
 update Users
 set EmailConfirmed = 0
 where UserId in ('4A7B05F2-AA55-45A4-608F-08DBCE615228', '75B0E499-C474-4BAF-560A-08DBCF2153E3', '8DA3C5D5-C608-48DD-560C-08DBCF2153E3')
---Напишите запрос для получения количества пользователей с неподтвержденными письмами;
+--Write a query to get count of users with unconfirmed emails;
 select count(UserId) as [Number of unverified postal addresses]
 from Users
 where EmailConfirmed = 0
---Напишите запрос для получения всех незавершенных задач (todos);
+--Write a query to get all uncompleted tasks (todos);-ГЌГ ГЇГЁГёГЁГІГҐ Г§Г ГЇГ°Г®Г± Г¤Г«Гї ГЇГ®Г«ГіГ·ГҐГ­ГЁГї ГўГ±ГҐГµ Г­ГҐГ§Г ГўГҐГ°ГёГҐГ­Г­Г»Гµ Г§Г Г¤Г Г· (todos);
 select *
 from Todos
 where IsCompleted = 0
---Напишите запрос, чтобы получить пользователей, у которых есть хотя бы 3 незавершенные задачи.
+--Write a query to get users who have at least 3 uncompleted tasks.
 select u.UserId, u.UserName, u.Email, count(u.UserId) as [Number not completed tasks]
 from Users as u
 join Todos as td on u.UserId = td.UserId
